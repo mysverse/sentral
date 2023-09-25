4df1a0d5cc24d11f42b0e9a8297567a868dd77c5
Tan Jun Yan <yan@yan3321.com>
2023-09-26 02:20:41 +0800

    feat: improve animation logic

--- app/auth/login/loginElement.tsx
+++ app/auth/login/loginElement.tsx
@@ -1,76 +1,107 @@
 "use client";
 
+import { Transition } from "@headlessui/react";
 import SignInButton from "components/signIn";
-import DefaultTransitionLayout from "components/transition";
+// import DefaultTransitionLayout from "components/transition";
 
 import Link from "next/link";
 
 import Logo from "public/img/MYSverse_Sentral_Logo.svg";
 import { useEffect, useState } from "react";
 
-export default function LoginElement() {
+export default function LoginElement({
+  imageLoaded
+}: {
+  imageLoaded: boolean;
+}) {
   const [show, setShow] = useState<boolean>(false);
+  const [authenticating, setAuthenticating] = useState<boolean>(false);
   useEffect(() => {
-    setShow(true);
-  }, []);
+    if (imageLoaded) {
+      setShow(true);
+    }
+  }, [imageLoaded]);
   return (
-    <DefaultTransitionLayout show={show} appear={false}>
-      <div>
-        <Logo className="h-20 w-auto fill-white" />
-        <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-100">
-          Welcome to Sentral (Beta)
-        </h2>
-        <p className="mt-3 text-sm leading-6 text-gray-200">
-          The official companion web app for{" "}
-          <Link
-            href="https://mysver.se/"
-            className="font-semibold text-gray-100 underline hover:text-gray-200 hover:no-underline"
-          >
-            MYSverse
-          </Link>{" "}
-          and MYSverse Sim.
+    <div className="relative flex h-full items-center justify-center">
+      <Transition
+        show={authenticating}
+        enter="transition duration-[500ms] delay-200"
+        enterFrom="opacity-0 scale-50"
+        enterTo="opacity-100 scale-100"
+        className="absolute"
+      >
+        <p className="text-xl font-medium leading-6 text-white">
+          Redirecting to Roblox...
         </p>
-        <p className="mt-3 text-sm leading-6 text-gray-200">
-          This site uses the Roblox Open Cloud and OAuth 2.0 APIs for data
-          fetching and authentication, which are still in beta.
-        </p>
-        <p className="mt-3 text-sm leading-6 text-gray-200">
-          {"Don't have a Roblox account? "}
-          <Link
-            href="https://www.roblox.com/"
-            className="font-semibold text-gray-100 underline hover:text-gray-200 hover:no-underline"
-          >
-            Sign up here!
-          </Link>
-        </p>
-      </div>
-      <div className="mt-10">
+      </Transition>
+
+      <Transition
+        show={show}
+        enter="transform transition duration-[500ms] delay-75"
+        enterFrom="opacity-0 -translate-y-36 scale-80"
+        enterTo="opacity-100 translate-y-0 scale-100"
+        leave="transform transition duration-[750ms]"
+        leaveFrom="opacity-100 translate-y-0 scale-100"
+        leaveTo="opacity-0 -translate-y-36 scale-80"
+      >
+        <div>
+          <Logo className="h-20 w-auto fill-white" />
+          <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-100">
+            Welcome to Sentral (Beta)
+          </h2>
+          <p className="mt-3 text-sm leading-6 text-gray-200">
+            The official companion web app for{" "}
+            <Link
+              href="https://mysver.se/"
+              className="font-semibold text-gray-100 underline hover:text-gray-200 hover:no-underline"
+            >
+              MYSverse
+            </Link>{" "}
+            and MYSverse Sim.
+          </p>
+          <p className="mt-3 text-sm leading-6 text-gray-200">
+            This site uses the Roblox Open Cloud and OAuth 2.0 APIs for data
+            fetching and authentication, which are still in beta.
+          </p>
+          <p className="mt-3 text-sm leading-6 text-gray-200">
+            {"Don't have a Roblox account? "}
+            <Link
+              href="https://www.roblox.com/"
+              className="font-semibold text-gray-100 underline hover:text-gray-200 hover:no-underline"
+            >
+              Sign up here!
+            </Link>
+          </p>
+        </div>
         <div className="mt-10">
           <div
-            onClick={() => setShow(false)}
+            onClick={() => {
+              setShow(false);
+              setAuthenticating(true);
+            }}
             className="mt-6 grid grid-cols-1 gap-4"
           >
             <SignInButton />
           </div>
         </div>
-      </div>
 
-      <p className="mt-10 text-sm leading-6 text-gray-300">
-        {"Read our "}
-        <Link
-          href="/privacy-policy"
-          className="font-medium text-gray-200 underline hover:text-gray-300 hover:no-underline"
-        >
-          privacy policy
-        </Link>
-        {" and "}
-        <Link
-          href="/terms-of-service"
-          className="font-medium text-gray-200 underline hover:text-gray-300 hover:no-underline"
-        >
-          terms of service
-        </Link>
-      </p>
-    </DefaultTransitionLayout>
+        <p className="mt-10 text-sm leading-6 text-gray-300">
+          {"Read our "}
+          <Link
+            href="/privacy-policy"
+            className="font-medium text-gray-200 underline hover:text-gray-300 hover:no-underline"
+          >
+            privacy policy
+          </Link>
+          {" and "}
+          <Link
+            href="/terms-of-service"
+            className="font-medium text-gray-200 underline hover:text-gray-300 hover:no-underline"
+          >
+            terms of service
+          </Link>
+        </p>
+      </Transition>
+    </div>
   );
 }
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,20 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import FeatureImage from "public/img/bandar_feature_image.webp";
 import LoginElement from "./loginElement";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 export default function LoginPage() {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
   return (
     <Suspense>
       <div className="flex min-h-full flex-1">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <LoginElement />
+          <div className="mx-auto h-full w-full max-w-sm lg:w-96">
+            <LoginElement imageLoaded={imageLoaded} />
           </div>
         </div>
         <div className="relative hidden w-0 flex-1 lg:block">
           <Image
             className="absolute inset-0 h-full w-full object-cover"
+            onLoadingComplete={() => {
+              setImageLoaded(true);
+            }}
             src={FeatureImage}
             alt="Feature image of Bandar"
             width={1919}
