2ef9500141a50ff05a0ea9a72019e4075ad2a78c
Tan Jun Yan <yan@yan3321.com>
2023-12-27 23:53:54 +0800

    fix: disable service worker locally due to instability

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
-  swDest: "public/sw.js"
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development"
 });
 
 export default withSerwist({
