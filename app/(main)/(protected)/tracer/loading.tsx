5afb69e7a5473810d4f1e0e35729c33fc5036b6a
Tan Jun Yan <yan@yan3321.com>
2024-01-08 03:33:13 +0800

    feat: refine layout and loading spinner

--- app/(main)/(protected)/tracer/loading.tsx
+++ app/(main)/(protected)/tracer/loading.tsx
@@ -1,4 +1,14 @@
+import Spinner from "components/spinner";
+
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div className="h-screen">Loading dev...</div>;
+  return (
+    <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+      <div className="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+        <div className="h-screen px-5 py-32">
+          <Spinner />
+        </div>
+      </div>
+    </div>
+  );
 }
