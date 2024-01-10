0edf168528abf99ff786f6491cb2b7f97b8df7b4
Tan Jun Yan <yan@yan3321.com>
2024-01-11 02:14:36 +0800

    refactor: consolidate loading ui

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
       <div className="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
-        <div className="h-screen px-5 py-32">
+        <div className="h-96 px-5 py-32">
           <Spinner />
         </div>
       </div>
