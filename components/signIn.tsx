b8d3edac8b438f6b4110a2424705097c72588055
Tan Jun Yan <yan@yan3321.com>
2024-03-18 20:19:46 +0800

    feat: redirect to callbackurl after login

--- components/signIn.tsx
+++ components/signIn.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 import RobloxIcon from "public/img/Roblox_Logo.svg";
 
 function SignInButton() {
   const { data: session } = useSession();
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
   if (session) {
     return (
       <button
@@ -20,7 +24,11 @@ function SignInButton() {
   return (
     <button
       type="button"
-      onClick={() => signIn("roblox")}
+      onClick={() =>
+        signIn("roblox", {
+          callbackUrl: callbackUrl ?? undefined
+        })
+      }
       className="inline-flex items-center justify-center gap-x-1.5 rounded-md bg-blue-700 px-2.5 py-2.5 text-sm font-semibold text-white shadow-md shadow-gray-200/10 outline outline-1 outline-gray-200 hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <RobloxIcon className="mx-2 h-5 w-5 fill-white" aria-hidden="true" />
