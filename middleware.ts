95eb89c30ad369d846c646e735c55c6725680583
Tan Jun Yan <yan@yan3321.com>
2024-10-26 05:28:19 +0800

    feat: correct malformed urls in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,27 @@
 import { NextResponse } from "next/server";
 import { auth } from "auth";
-
 import { NextAuthRequest } from "next-auth/lib";
 
 export default auth(async (request: NextAuthRequest) => {
-  // If the user is at the root path and authenticated, redirect to /dashboard
-  if (!request.auth) {
-    const url = request.nextUrl.clone();
+  const url = request.nextUrl.clone();
+
+  // Remove unwanted characters like ")", "*", etc., from the path
+  const sanitizedPath = url.pathname.replace(/[^\w/-]/g, "");
+  if (sanitizedPath !== url.pathname) {
+    url.pathname = sanitizedPath;
+    return NextResponse.redirect(url); // Redirect only if the path was actually modified
+  }
+
+  // Redirect unauthenticated users to login, except if they are already on login page
+  if (!request.auth && url.pathname !== "/auth/login") {
     const originalPathname = url.pathname;
     url.pathname = `/auth/login`;
     url.searchParams.set("callbackUrl", originalPathname);
     return NextResponse.redirect(url);
   }
 
-  if (request.nextUrl.pathname === "/" && request.auth) {
-    const url = request.nextUrl.clone();
+  // Redirect authenticated users from root to /dashboard, only if not already there
+  if (request.auth && url.pathname === "/") {
     url.pathname = "/dashboard";
     return NextResponse.redirect(url);
   }
