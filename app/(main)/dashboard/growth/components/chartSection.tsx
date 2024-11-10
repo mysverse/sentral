5ee3ef7884c68824208501967d0c50d490af0836
Tan Jun Yan <yan@yan3321.com>
2024-11-10 13:19:36 +0800

    fix: growth dropdown opening to the side

--- app/(main)/dashboard/growth/components/chartSection.tsx
+++ app/(main)/dashboard/growth/components/chartSection.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { ArrowsUpDownIcon, CheckIcon } from "@heroicons/react/20/solid";
+import { Fragment, useCallback, useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
 import {
   Label,
   Listbox,
@@ -11,10 +13,8 @@ import {
   RadioGroup,
   Transition
 } from "@headlessui/react";
-import { clsx } from "clsx";
-import { Fragment, useCallback, useEffect, useState } from "react";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import dynamic from "next/dynamic";
+import { ArrowsUpDownIcon, CheckIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
 const chartAxisOptions = [
   { name: "Linear", value: "linear", disabled: false },
@@ -44,13 +44,10 @@ export default function GrowthChartSection({
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  // Get a new searchParams string by merging the current
-  // searchParams with a provided key/value pair
   const createQueryString = useCallback(
     (name: string, value: string) => {
       const params = new URLSearchParams(searchParams.toString());
       params.set(name, value);
-
       return params.toString();
     },
     [searchParams]
@@ -89,7 +86,6 @@ export default function GrowthChartSection({
   return (
     <>
       <div className="rounded-lg bg-white px-1 py-6 shadow sm:px-5">
-        {/* chart won't scale properly without width class: https://stackoverflow.com/a/70191511 */}
         <div className="relative h-[28rem] w-[99%]">
           <GrowthChart
             chartData={chartData}
@@ -111,21 +107,21 @@ export default function GrowthChartSection({
               <Radio
                 key={option.name}
                 value={option.value}
-                className={({ focus, checked }) =>
+                className={({ hover, checked }) =>
                   clsx(
+                    "flex items-center justify-center rounded-md border px-2 py-3 text-sm font-medium sm:flex-1 sm:px-6",
                     !option.disabled
                       ? "cursor-pointer focus:outline-none"
                       : "cursor-not-allowed opacity-25",
-                    focus ? "ring-2 ring-blue-500 ring-offset-2" : "",
                     checked
                       ? "border-transparent bg-blue-600 text-white hover:bg-blue-700"
                       : "border-gray-200 bg-white text-gray-900 hover:bg-gray-50",
-                    "flex items-center justify-center rounded-md border px-2 py-3 text-sm font-medium sm:flex-1 sm:px-6"
+                    hover ? "ring-2 ring-blue-500 ring-offset-2" : ""
                   )
                 }
                 disabled={option.disabled}
               >
-                <Label as="p">{option.name}</Label>
+                <Label>{option.name}</Label>
               </Radio>
             ))}
           </div>
@@ -146,52 +142,52 @@ export default function GrowthChartSection({
           }}
         >
           {({ open }) => (
-            <>
-              <div className="relative my-auto flex flex-grow">
-                <ListboxButton className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-3 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
-                  <div className="flex items-center">
-                    <span
-                      aria-label={selectedMonth.valid ? "Online" : "Offline"}
-                      className={clsx(
-                        selectedMonth.valid ? "bg-green-400" : "bg-gray-200",
-                        "inline-block h-2 w-2 flex-shrink-0 rounded-full"
-                      )}
-                    />
-                    <span className="ml-3 block truncate">
-                      {selectedMonth.name}
-                    </span>
-                  </div>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ArrowsUpDownIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
+            <div className="relative">
+              <ListboxButton className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-3 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+                <div className="flex items-center">
+                  <span
+                    aria-label={selectedMonth.valid ? "Online" : "Offline"}
+                    className={clsx(
+                      selectedMonth.valid ? "bg-green-400" : "bg-gray-200",
+                      "inline-block h-2 w-2 flex-shrink-0 rounded-full"
+                    )}
+                  />
+                  <span className="ml-3 block truncate">
+                    {selectedMonth.name}
                   </span>
-                </ListboxButton>
+                </div>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <ArrowsUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </ListboxButton>
 
-                <Transition
-                  show={open}
-                  as="div"
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
+              <Transition
+                as="div"
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                {open && (
                   <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                     {months.map((month, index) => (
                       <ListboxOption
                         key={index}
-                        className={({ focus }) =>
-                          clsx(
-                            focus
-                              ? "bg-gradient-to-l from-blue-500 via-blue-700 to-blue-800 text-white"
-                              : "text-gray-900",
-                            "relative cursor-default select-none py-2 pl-3 pr-9"
-                          )
-                        }
                         value={month.value}
                         disabled={!month.valid}
+                        className={({ selected, disabled }) =>
+                          clsx(
+                            "relative cursor-default select-none py-2 pl-3 pr-9",
+                            selected
+                              ? "bg-gradient-to-l from-blue-500 via-blue-700 to-blue-800 text-white"
+                              : "text-gray-900",
+                            disabled && "opacity-50"
+                          )
+                        }
                       >
-                        {({ selected, focus }) => (
+                        {({ selected }) => (
                           <>
                             <div className="flex items-center">
                               <span
@@ -214,11 +210,10 @@ export default function GrowthChartSection({
                                 </span>
                               </span>
                             </div>
-
                             {selected ? (
                               <span
                                 className={clsx(
-                                  focus ? "text-white" : "text-slate-600",
+                                  selected ? "text-white" : "text-blue-600",
                                   "absolute inset-y-0 right-0 flex items-center pr-4"
                                 )}
                               >
@@ -233,9 +228,9 @@ export default function GrowthChartSection({
                       </ListboxOption>
                     ))}
                   </ListboxOptions>
-                </Transition>
-              </div>
-            </>
+                )}
+              </Transition>
+            </div>
           )}
         </Listbox>
       </div>
