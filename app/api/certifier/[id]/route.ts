c9933f26f13a6ded3ff8b7ab47fd2c32dd3631a5
Tan Jun Yan <yan@yan3321.com>
2024-11-15 22:45:13 +0800

    fix: use correct next request typings

--- app/api/certifier/[id]/route.ts
+++ app/api/certifier/[id]/route.ts
@@ -1,23 +1,21 @@
-import type { NextApiRequest } from "next";
+import type { NextRequest } from "next/server";
 import { pdf } from "@react-pdf/renderer";
 import { renderCertificateById } from "app/(main)/dashboard/certifier/actions";
 
 export async function GET(
-  req: NextApiRequest,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const Certificate = await renderCertificateById(params.id);
-  if (Certificate) {
+  const filename = "mysverse_certificate.pdf";
+  const certificate = await renderCertificateById(params.id);
+  if (certificate) {
     const instance = pdf();
-    instance.updateContainer(Certificate);
+    instance.updateContainer(certificate);
     const fileBlob = await instance.toBlob();
     // This is the key part - set the headers to tell the browser to download the file
     const headers = new Headers();
     // remember to change the filename here
-    headers.append(
-      "Content-Disposition",
-      'attachment; filename="certificater.pdf"'
-    );
+    headers.append("Content-Disposition", `attachment; filename="${filename}"`);
     headers.append("Content-Type", "application/pdf");
     return new Response(fileBlob, {
       headers
