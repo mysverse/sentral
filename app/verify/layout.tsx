bc62d54c43bf075026f093d149632f497cd7723a
Tan Jun Yan <yan@yan3321.com>
2024-11-16 03:33:17 +0800

    fix: wrap verify layout in suspense

--- app/verify/layout.tsx
+++ app/verify/layout.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
         <div className="flex min-h-dvh flex-row items-center justify-center bg-gradient-to-l from-blue-500 via-blue-700 to-blue-800">
-          {children}
+          <Suspense>{children}</Suspense>
         </div>
       </body>
     </html>
