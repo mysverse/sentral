1fcc5b2ec89ab69b9ee2ffb54afbe52f1b0772b6
Tan Jun Yan <yan@yan3321.com>
2024-11-16 04:38:31 +0800

    feat: apply bg colour on html instead of div

--- app/verify/layout.tsx
+++ app/verify/layout.tsx
@@ -2,11 +2,9 @@ import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <body>
-        <div className="flex min-h-dvh flex-row items-center justify-center bg-gradient-to-l from-blue-500 via-blue-700 to-blue-800">
-          <Suspense>{children}</Suspense>
-        </div>
+    <html className="bg-gradient-to-l from-blue-500 via-blue-700 to-blue-800">
+      <body className="flex min-h-dvh flex-row items-center justify-center">
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
