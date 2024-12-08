510e02e4c6ad05a3777cf94b2af313d325f21001
Tan Jun Yan <yan@yan3321.com>
2024-12-08 17:13:38 +0800

    feat: implement clerk auth

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,5 @@
 import LoginContent from "./loginContent";
 
 export default async function LoginPage() {
-  // const session = await auth();
-  // if (session) {
-  //   return redirect("/");
-  // }
   return <LoginContent />;
 }
