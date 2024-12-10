821fb214f5c2ba40853a2a98d29b1035f8393afb
Tan Jun Yan <yan@yan3321.com>
2024-12-10 23:55:51 +0800

    fix: more service worker fixes

--- utils/notification.ts
+++ utils/notification.ts
@@ -1,14 +1,22 @@
 "use client";
 
-export function notify(title: string, options?: NotificationOptions) {
+export async function notify(title: string, options?: NotificationOptions) {
   try {
     new Notification(title, options);
   } catch (error: unknown) {
     if (error instanceof TypeError) {
       console.warn("Error showing notification", error);
     }
-    void navigator.serviceWorker.ready.then(({ showNotification }) => {
+    try {
+      const registration = await navigator.serviceWorker.ready;
+      const { showNotification, pushManager } = registration;
+      let subscription = await pushManager.getSubscription();
+      if (!subscription) {
+        subscription = await pushManager.subscribe();
+      }
       showNotification(title, options);
-    });
+    } catch (e) {
+      console.error("Error showing notification via service worker", e);
+    }
   }
 }
